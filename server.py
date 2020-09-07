a877d02a44a917e1f2de0ee936b387c60dd7c8b3
Soroush Rasti <rasti.soroush@gmail.com>
2020-09-07 18:34:42 +0200

    adding back-end for contact form

--- server.py
+++ server.py
@@ -1,15 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app=Flask( __name__ )
 
-@app.route('/welcome')
-def hello():
-    return 'welcome to my website'
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
 
 
-@app.route('/')
-def first():
-    return render_template('index.html' )
+@app.route('/submit_form', methods=['POST','GET'])
+def submit_form():
+    if request.method=='POST':
+       data=request.form.to_dict()
+       return data
+
 
 
 
