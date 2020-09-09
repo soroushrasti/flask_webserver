a7184cb5ce64eb3bf4e54f2a606fa291557d9008
Soroush Rasti <rasti.soroush@gmail.com>
2020-09-09 18:49:00 +0200

    adding backend-csv_writer

--- server.py
+++ server.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request,redirect
+import csv
 app=Flask( __name__ )
+
+@app.route('/')
+def home_html():
+    return render_template('index.html')
 
 @app.route('/<string:page_name>')
 def html_page(page_name):
@@ -10,8 +14,24 @@ def html_page(page_name):
 @app.route('/submit_form', methods=['POST','GET'])
 def submit_form():
     if request.method=='POST':
-       data=request.form.to_dict()
-       return data
+        try:
+            data=request.form.to_dict()
+            write_to_csv(data)
+            return redirect('./Thankyou.html')
+        except:
+            return 'I problem in database'    
+
+def write_to_csv(data):
+     with open('database.csv', newline='',mode='a') as file_csv:
+         email=data['email']
+         subject=data['subject']
+         message=data['message']
+         csv_writer=csv.writer(file_csv)
+         csv_writer.writerow([email,subject,message])
+
+  
+
+
 
 
 
